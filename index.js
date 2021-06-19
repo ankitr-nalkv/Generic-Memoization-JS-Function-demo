5ed18c989ac3838636aba91d99f9a41c8616bda1
ankitr-nalkv <ankitr.nalkv@gmail.com>
2021-06-19 23:28:42 +0530

    Added generic Memoization function as decorator

--- index.js
+++ index.js
@@ -4,3 +4,44 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function memoize(fn) {
+  let cache = {};
+  return (...args) => {
+    const key = [...args].join('+');
+    if (cache.hasOwnProperty(key)) {
+      console.log('cached', key);
+      return cache[key];
+    } else {
+      console.log('computed', key);
+      cache[key] = fn(...args);
+      return cache[key];
+    }
+  };
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+var sum = memoize(sum);
+
+console.log(sum(2, 3));
+console.log(sum(4, 5));
+console.log(sum(2, 3));
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+var fibonacci = memoize(fibonacci);
+
+console.log(fibonacci(5));
+console.log(fibonacci(10));
+console.log(fibonacci(5));
